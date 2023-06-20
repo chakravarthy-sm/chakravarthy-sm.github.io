33a8c46d0641f3a191f5d1798eaa4bab593131f8
Chakravarthy S M <chakrava@synopsys.com>
2023-06-20 19:46:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Welcome</p>
+        <div id="container">
+<table>
+  <th>Foo</th>
+  <th>Bar</th>
+  <tr>Chakra</tr>
+  <tr>Mithun</tr>
+  <tr>SM</tr>
+</table>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
